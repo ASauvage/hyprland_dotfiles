145a7952e20504ac6e58e5e5b4c5e8c21c09defd
Alexandre SAUVAGE POTEL <alexandre.sauvage-potel@ratpsmartsystems.com>
2025-05-12 13:40:56 +0200

    [feature] ask confirmation for config backup

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -3,9 +3,14 @@
 # This script installs the dotfiles.
 
 if [ -d "$HOME/.config" ]; then
-    echo ":: Backed up existing .config directory to .config.bak"
-    cp -r "$HOME/.config" "$HOME/.config.bak"
+    echo ":: .config directory already exists"
+    if gum confirm "Back up existing config?"; then
+        echo ":: Backed up existing .config directory to .config.bak"
+        cp -r "$HOME/.config" "$HOME/.config.bak"
+    else
+        echo ":: Skipping Back up existing config?."
+    fi
 fi
 
-mv -rf ./dotfiles/* "$HOME"/.config/
+mv -rf ./dotfiles/* "$HOME"/
 echo ":: New config installed"
