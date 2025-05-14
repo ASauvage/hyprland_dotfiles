1d12e2fb98873cc283824ff3eb05325cdbbcd726
ASauvage <a.sauvagepotel@gmail.com>
2025-05-14 13:41:03 +0200

    [feature] wallpaper setup script

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -2,5 +2,10 @@
 
 #  This script sets up the wallpaper for Hyprland using swww.
 
-cp ../wallpapers/* ~/Pictures/wallpapers/
+echo ":: Setting up wallpaper for Hyprland using swww"
+echo ":: Copying wallpapers to ~/Pictures/wallpapers"
+
+cp -v ../wallpapers/* ~/Pictures/wallpapers/
 cp ../wallpapers/$(gum choose --header "Select a wallpaper" --height 20 $(ls ./wallpapers)) ~/Pictures/wallpaper.jpg
+
+echo ":: Successfully set wallpaper"
